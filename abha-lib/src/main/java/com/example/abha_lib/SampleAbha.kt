a2d3fe6e8b2745233aec45fb2e8feee5010a89d0
SanoferSameera <sanofersameera25@gmail.com>
2023-06-27 18:07:03 +0530

    Add. Created a new module for library

---
+++ abha-lib/src/main/java/com/example/abha_lib/SampleAbha.kt
@@ -0,0 +1,24 @@
+package com.example.abha_lib
+
+class SampleAbha {
+    companion object {
+        var SESSION_TOKEN: String = "abcd"
+//        var PATIENT_JSON : JSONObject = JSONObject("{\n" +
+//                "  \"Subject type\": \"Patient\",\n" +
+//                "  \"External ID\": null,\n" +
+//                "  \"Voided\": false,\n" +
+//                "  \"Registration date\": \"2023-06-22\",\n" +
+//                "  \"location\": {\n" +
+//                "    \"State\": \"Hosakote\",\n" +
+//                "    \"State External ID\": null\n" +
+//                "  },\n" +
+//                "  \"First name\": \"Priya\",\n" +
+//                "  \"Last name\": \"Rao\",\n" +
+//                "  \"Date of birth\": \"2013-03-22\",\n" +
+//                "  \"Gender\": \"Female\",\n" +
+//                "  \"observations\": {\n" +
+//                "  },\n" +
+//                "  \"Address\": \"Hosakote\"\n" +
+//                "}\n")
+    }
+}
